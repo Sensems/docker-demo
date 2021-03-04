fb269d76ec38fb94fd8c17ec0973ab532f8740d4
sensems <1187760012@qq.com>
2021-03-04 18:08:05 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const http = require("http")
-const {
-  execSync
-} = require("child_process")
-const path = require("path")
-const fs = require("fs")
+const http = require("http");
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 // 递归删除目录
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function (file) {
+    fs.readdirSync(path).forEach(function(file) {
       const curPath = path + "/" + file;
-      if (fs.statSync(curPath).isDirectory()) { // recurse
+      if (fs.statSync(curPath).isDirectory()) {
+        // recurse
         deleteFolderRecursive(curPath);
-      } else { // delete file
+      } else {
+        // delete file
         fs.unlinkSync(curPath);
       }
     });
@@ -20,10 +20,10 @@ function deleteFolderRecursive(path) {
   }
 }
 
-const resolvePost = req =>
-  new Promise(resolve => {
+const resolvePost = (req) =>
+  new Promise((resolve) => {
     let chunk = "";
-    req.on("data", data => {
+    req.on("data", (data) => {
       chunk += data;
     });
     req.on("end", () => {
@@ -31,44 +31,62 @@ const resolvePost = req =>
     });
   });
 
-http.createServer(async (req, res) => {
-  console.log('receive request')
-  console.log(req.url)
-  if (req.method === 'POST' && req.url === '/') {
-    const data = await resolvePost(req);
-    const projectDir = path.resolve(`./${data.repository.name}`)
-    deleteFolderRecursive(projectDir)
+http
+  .createServer((req, res) => {
+    console.log("receive request");
+    console.log(req.url);
+    if (req.method === "POST" && req.url === "/") {
+      resolvePost(req).then((data) => {
+        const projectDir = path.resolve(`./${data.repository.name}`);
+        deleteFolderRecursive(projectDir);
 
-    // 拉取仓库最新代码
-    execSync(`git clone https://github.com/yeyan1996/${data.repository.name}.git ${projectDir}`, {
-      stdio: 'inherit',
-    })
+        // 拉取仓库最新代码
+        execSync(
+          `git clone https://github.com/Sensems/${data.repository.name}.git ${projectDir}`,
+          {
+            stdio: "inherit",
+          }
+        );
 
-    // 复制 Dockerfile 到项目目录
-    fs.copyFileSync(path.resolve(`./Dockerfile`), path.resolve(projectDir, './Dockerfile'))
+        // 复制 Dockerfile 到项目目录
+        fs.copyFileSync(
+          path.resolve(`./Dockerfile`),
+          path.resolve(projectDir, "./Dockerfile")
+        );
 
-    // 复制 .dockerignore 到项目目录
-    fs.copyFileSync(path.resolve(__dirname, `./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
+        // 复制 .dockerignore 到项目目录
+        fs.copyFileSync(
+          path.resolve(__dirname, `./.dockerignore`),
+          path.resolve(projectDir, "./.dockerignore")
+        );
 
-    // 创建 docker 镜像
-    execSync(`docker build . -t ${data.repository.name}-image:latest `, {
-      stdio: 'inherit',
-      cwd: projectDir
-    })
+        // 创建 docker 镜像
+        execSync(`docker build . -t ${data.repository.name}-image:latest `, {
+          stdio: "inherit",
+          cwd: projectDir,
+        });
 
-    // 销毁 docker 容器
-    execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`, {
-      stdio: 'inherit',
-    })
+        // 销毁 docker 容器
+        execSync(
+          `docker ps -a -f "name=^${data.repository.name}-container" --format="{{.Names}}" | xargs -r docker stop | xargs -r docker rm`,
+          {
+            stdio: "inherit",
+          }
+        );
 
-    // 创建 docker 容器
-    execSync(`docker run -d -p 8888:80 --name ${data.repository.name}-container  ${data.repository.name}-image:latest`, {
-      stdio: 'inherit',
-    })
+        // 创建 docker 容器
+        execSync(
+          `docker run -d -p 8888:80 --name ${data.repository.name}-container  ${data.repository.name}-image:latest`,
+          {
+            stdio: "inherit",
+          }
+        );
 
-    console.log('deploy success')
-    res.end('ok')
-  }
-}).listen(3000, () => {
-  console.log('server is ready')
-})
+        console.log("deploy success");
+        res.end("ok");
+      });
+    }
+  })
+  .listen(3000, () => {
+    console.log("server is ready");
+  });
